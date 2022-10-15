4417a74b688385d4be81c36e73029b5d7b20a7fd
gnendraaw <itsgnendraaw@gmail.com>
2022-10-16 02:34:44 +0800

    add homepage and detail page

---
+++ lib/detail_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/data/articles.dart';
+
+class DetailPage extends StatelessWidget {
+  static const routeName = '/article_detail';
+
+  final Article article;
+
+  const DetailPage({Key? key, required this.article}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(article.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.network(
+              article.urlToImage,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(18),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(article.description),
+                  const Divider(color: Colors.grey),
+                  Text(
+                    article.title,
+                    style: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    article.content,
+                  ),
+                  const Divider(color: Colors.grey),
+                  ElevatedButton(
+                    child: const Text('Read More'),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/data/articles.dart';
+import 'package:news_app/detail_page.dart';
+
+class HomePage extends StatelessWidget {
+  static const routeName = '/home_page';
+
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('News App'),
+      ),
+      body: FutureBuilder(
+        future:
+            DefaultAssetBundle.of(context).loadString('assets/articles.json'),
+        builder: ((context, snapshot) {
+          final List<Article> articles = parseArticle(snapshot.data);
+          return ListView.builder(
+            itemCount: articles.length,
+            itemBuilder: (context, index) {
+              return _buildArticleItem(context, articles[index]);
+            },
+          );
+        }),
+      ),
+    );
+  }
+}
+
+Widget _buildArticleItem(BuildContext context, Article article) {
+  return ListTile(
+      leading: Image.network(
+        article.urlToImage,
+        width: 100,
+      ),
+      title: Text(article.title),
+      subtitle: Text(article.author),
+      onTap: () {
+        Navigator.pushNamed(context, DetailPage.routeName, arguments: article);
+      });
+}
